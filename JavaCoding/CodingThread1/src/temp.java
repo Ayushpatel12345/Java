33f8614add832ee6b4a7d2bf40baaf5f2bc002fd
Ayushpatel12345 <141323380+Ayushpatel12345@users.noreply.github.com>
2024-06-22 14:02:07 +0530

    Add files via upload

---
+++ JavaCoding/CodingThread1/src/temp.java
@@ -0,0 +1,9 @@
+public class temp {
+    public static void main(String[] args) {
+//        int a=20,b=0;
+//        System.out.println(a/b);
+
+        int a1=10;
+        System.out.println(a1);
+    }
+}
