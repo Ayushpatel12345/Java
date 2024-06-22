3cbaf7795ed5c89705f75aa68a7cffe60f00d655
Ayushpatel12345 <141323380+Ayushpatel12345@users.noreply.github.com>
2024-06-22 13:53:15 +0530

    Add files via upload

---
+++ JavaCoding/CodingDocx1/src/JavaDocx2.java
@@ -0,0 +1,17 @@
+/**
+ * @author
+ * @version 0.1
+ * @since 2002
+ * @see JavaDocx1
+ */
+class sam{
+    static public int add(int a,int b){
+        return a*b;
+    }
+}
+public class JavaDocx2  {
+    public static void main(String[] args) {
+
+        System.out.println(a.add(10,20));
+    }
+}
