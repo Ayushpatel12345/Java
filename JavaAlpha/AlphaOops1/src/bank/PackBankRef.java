c2dcf377e547a7adee643734ebd86084a391f401
Ayushpatel12345 <141323380+Ayushpatel12345@users.noreply.github.com>
2024-08-03 10:48:15 +0530

    Delete JavaAlpha directory

--- JavaAlpha/AlphaOops1/src/bank/PackBankRef.java
+++
@@ -1,9 +0,0 @@
-package bank;
-
-public class PackBankRef {
-    public static void main(String[] args) {
-        bank.Account ac1 = new bank.Account();
-        ac1.name = "cust1";
-        System.out.println(ac1.name);
-    }
-}
