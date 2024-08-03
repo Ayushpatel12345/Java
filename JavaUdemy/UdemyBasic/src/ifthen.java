e339ba93d990d1c6185143f11093a4a2dba82195
Ayushpatel12345 <141323380+Ayushpatel12345@users.noreply.github.com>
2024-08-03 10:50:19 +0530

    Delete JavaUdemy directory

--- JavaUdemy/UdemyBasic/src/ifthen.java
+++
@@ -1,8 +0,0 @@
-public class ifthen {
-    public static void main(String[] args) {
-        boolean isDev = false;
-        if(isDev==false) {
-            System.out.println("it is not dev");
-        }
-    }
-}
--- JavaUdemy/UdemyOpps2/src/POLYmovieref.java
+++
@@ -1,7 +0,0 @@
-public class POLYmovieref {
-    public static void main(String[] args) {
-
-        POLYmovie theMovie = POLYmovie.getPOLYmovie("Science","star wars");
-        theMovie.watchMovie();
-    }
-}
