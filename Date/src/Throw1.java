13a5f518575dcb3a76bf696f8a2e17c3174d6bc3
Ayushpatel12345 <141323380+Ayushpatel12345@users.noreply.github.com>
2024-08-03 10:55:52 +0530

    Delete Date directory

--- Date/src/Throw1.java
+++
@@ -1,8 +0,0 @@
-public class Throw1 {
-    public static void example() {
-        throw new ArithmeticException("divide by 0");
-    }
-    public static void main(String[] args) {
-        example();
-    }
-}
