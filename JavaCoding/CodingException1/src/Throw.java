d225d017932f0ca9a5dfbc1f736c68ced9d3d9e9
Ayushpatel12345 <141323380+Ayushpatel12345@users.noreply.github.com>
2024-08-03 10:48:34 +0530

    Delete JavaCoding directory

--- JavaCoding/CodingException1/src/Throw.java
+++
@@ -1,6 +0,0 @@
-public class Throw {
-    public static void main(String[] args) throws Exception {
-
-        throw new Exception("Hello friends,Good morning");
-    }
-}
--- JavaCoding/CodingThread1/src/temp.java
+++
@@ -1,9 +0,0 @@
-public class temp {
-    public static void main(String[] args) {
-//        int a=20,b=0;
-//        System.out.println(a/b);
-
-        int a1=10;
-        System.out.println(a1);
-    }
-}
